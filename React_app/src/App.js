18b9a50413a759f18f39ae77a1a4d855b18f3691
Dmusketeer <ardmountronics@gmail.com>
2021-07-16 00:00:45 +0530

    import components in App js file

--- React_app/src/App.js
+++ React_app/src/App.js
@@ -1,35 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-const name="Dheeraj";
-const currentDate=new Date();
-const hours= currentDate.getHours();
-const year= currentDate.getFullYear();
-const randomColor={
-  color:""
-};
-
-let greeting;
-if(hours<12){
-  greeting="Good Morning";
-  randomColor.color="red";
-}
-else if(hours<18 ){
-  greeting = "Good afternoon";
-  randomColor.color = "green";
-}else{
-  greeting = "Good evening";
-  randomColor.color = "blue";
-}
+import Users from './Components/user.js';
+import Product from './Components/products.js';
+import Review from './Components/review.js';
 
 function App() {
   return (
-    <div className="App">
-      <h3 style={randomColor}>{greeting }</h3>
-      <h2>Hey {name}</h2>
-      <h2>current Year : {year}</h2>
-      <h1>happy Learning!</h1><span>:mic</span>
-
-    </div>
+    <>
+    <Users />
+    <Product />
+    <Review />
+    </>
   );
 }
 
