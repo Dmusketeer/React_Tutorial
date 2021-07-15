8c190db499517c6635c9794ff622ac2db029282b
Dmusketeer <ardmountronics@gmail.com>
2021-07-16 00:01:33 +0530

    components

---
+++ React_app/src/Components/products.js
@@ -0,0 +1,15 @@
+function product(){
+    return (
+        <>
+        <ul>
+            <li>Watch</li>
+            <li>Pen</li>
+            <li>Mobile</li>
+        </ul>
+        
+        </>
+
+    )
+}
+
+export default product;
---
+++ React_app/src/Components/review.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Review() {
+    return (
+        <div>
+            <h3>This Product is not bad!</h3>
+        
+        </div>
+    )
+}
