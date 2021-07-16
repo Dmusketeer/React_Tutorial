8d389a7678b900dade7287e1b53a39eb596c7ce2
Dmusketeer <ardmountronics@gmail.com>
2021-07-16 18:24:37 +0530

    state in class component

--- React_app/src/App.js
+++ React_app/src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
-import {useState} from 'react';  //useState is a hook for state
-// import Users from './Components/user.js';
-// import Product from './Components/products.js';
-// import Review from './Components/review.js';
+import React, {Component} from 'react';
 
-// const data="Dheeraj"
 
-function App() {
-//  const [data,setData] = useState("Name Please");
- const [data,setData] = useState(0);
 
- function updateData(){
-   setData(data+1);
- }
-  return (
-    <>
 
-    {/* <h1>Hello {data}!</h1> The value of data is maria now but it is not updated yet here.this is the reason we are using states and props */}
-    <h1>{data}</h1>
-    <button onClick={updateData}>Update</button> 
-    {/* <button onClick={()=>alert("Hello")}>Hello</button> */}
-    </>
-  );
+//class components:
+class App extends Component {
+// define states in class component
+constructor(){
+    super();
+    this.state={
+        data:0
+    }
 }
-
+Myname(){
+    this.setState({data:"Hey Buddy!"}); 
+    this.setState({data:this.state.data+1}); 
+}
+    render(){
+        return (
+            <div className="App">
+            <h1>{this.state.data}</h1>
+            <button onClick={()=>this.Myname()}>Update</button>
+            </div>
+        )
+    }
+}
 export default App;
