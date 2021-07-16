706c34d0e480f7e14566ec5307ceda2366291690
Dmusketeer <ardmountronics@gmail.com>
2021-07-16 08:45:29 +0530

    click event and functions

--- React_app/src/App.js
+++ React_app/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import Users from './Components/user.js';
-import Product from './Components/products.js';
-import Review from './Components/review.js';
+// import Users from './Components/user.js';
+// import Product from './Components/products.js';
+// import Review from './Components/review.js';
+
+// const data="Dheeraj"
 
 function App() {
+  let data="Dheeraj Tiwari";
+  function Change() {
+   return (
+      data="maria",
+      alert(data)
+    )
+  }
+
   return (
     <>
-    <Users />
-    <Product />
-    <Review />
+    <h1>Hello {data}!</h1> {/*The value of data is maria now but it is not updated yet here.this is the reason we are using states and props*/}
+    <button onClick={Change}>Click</button> 
+    {/* <button onClick={()=>alert("Hello")}>Hello</button> */}
     </>
   );
 }
