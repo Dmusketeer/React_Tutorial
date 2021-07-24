41bb4a0be0a096f3a68a6a6793b8fbdde1866014
Dmusketeer <ardmountronics@gmail.com>
2021-07-24 08:58:20 +0530

    static

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,47 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  
-
   //simple variables
   const faName="Dheeraj"
   const laName="Tiwari"
   const age=26
   const job="Developer"
   // const placeHolder="Enter FatherName"
-
   //array
   const Frinds=["raga","saga","maga"]
-
   //object
   const myFev={
     "game":"chess",
     "food":"samosa"
-
   }
-
   //function
   const placeHolder=()=>{
     return "Enter MotherName";
   }
   
+const blogObj={
+  "title": "Blog Title",
+  "description": " lorem sorem dorem heyy nouu laoiu mahfiu sanul ,hewroi ,qweyub ,qw3erwrfrtgferuhwqdjk "
+
+}
+
   return (
     <div className="App">
-      <h2>FullName : {`${faName} ${laName}`} </h2> 
+      {/* <h2>FullName : {`${faName} ${laName}`} </h2> 
       <h2>Age : {age} </h2> 
-      <h2>JOB : {job} </h2> 
+      <h2>JOB : {job} </h2>  */}
       {/* <input type="text" placeholder="Enter Name"/> */}
-      <input type="text" placeholder={placeHolder()}/>
+      {/* <input type="text" placeholder={placeHolder()}/>
       <h3>Best Friend : {Frinds[0]}</h3>
       <h2>MyFevorite Food : {myFev.food}</h2>
       <h2>MyFevorite Game : {myFev.game}</h2>
       <h2>{
         myFev.game==="chess"?"chess":"false"
-        }</h2>
-
+        }</h2> */}
+    <div className="blogCard">
+      <h3>{blogObj.title}</h3>
+      <p>{blogObj.description}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
