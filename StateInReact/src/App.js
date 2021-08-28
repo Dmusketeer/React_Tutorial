4b128a4f6abc30438a19ea8eec00e3a8d715a077
Dmusketeer <ardmountronics@gmail.com>
2021-08-28 12:35:28 +0530

    src folder

---
+++ StateInReact/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Car from './components/State'
+function App() {
+  return (
+    <div className="App">
+      <Car />
+    </div>
+  );
+}
+
+export default App;
---
+++ StateInReact/src/components/State.js
@@ -0,0 +1,40 @@
+// React components has a built -in state object.
+
+// The state object is where you store property values that belongs to the component.
+
+// When the state object changes, the component re - renders.
+
+import React, { Component } from 'react'
+class Car extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            brand: "Ford",
+            model: "Mustang",
+            color: "red",
+            year: 1964
+        };
+    }
+    changeColor = () => {
+        this.setState({ color: "blue" });
+    }
+    render() {
+        return (
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color}
+                    {this.state.model}
+                    from {this.state.year}.
+                </p>
+                <button
+                    type="button"
+                    onClick={this.changeColor}
+                >Change color</button>
+            </div>
+        );
+    }
+}
+
+
+export default Car;
