a16adea298f1aabace48e21d97c9860471034775
Dmuskeeter <tdheeraj9a@gmail.com>
2022-01-18 20:19:53 +0530

    src folder

---
+++ reactrouter/src/App.js
@@ -0,0 +1,13 @@
+import {Routes,Route} from 'react-router-dom'
+import {Home} from './components/Home'
+import {About} from './components/About'
+function App() {
+  return (
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+  </Routes>
+  );
+}
+
+export default App;
