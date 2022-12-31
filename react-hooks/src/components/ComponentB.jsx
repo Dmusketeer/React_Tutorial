90346e3b7bb1986c16d4476448561c16e0bfc221
Dmuskeeter <tdheeraj9a@gmail.com>
2022-12-31 19:40:45 +0530

    components

---
+++ react-hooks/src/components/ComponentB.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentC from './ComponentC'
+
+const ComponentB = () => {
+    return (
+        <div>
+            <ComponentC />
+        </div>
+    );
+}
+
+export default ComponentB;
