50cf5120a45e214cc5b88eb175ce9294cfef78fd
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-01 16:57:48 +0530

    useContext

--- react-hooks/src/components/ComponentC.jsx
+++ react-hooks/src/components/ComponentC.jsx
@@ -1,22 +1,22 @@
-import React,{useContext} from 'react';
-import {UserContext} from '../App';
+// import React,{useContext} from 'react';
+// import {UserContext} from '../App';
 
-const ComponentC = () => {
-    const user=useContext(UserContext)
-    return (
-        <div>
-            {/* <UserContext.Consumer>
-                {
-                   user=>{
-                    return <h1>My Name is {user}</h1>
-                   }
-                }
-            </UserContext.Consumer> */}
+// const ComponentC = () => {
+//     const user=useContext(UserContext)
+//     return (
+//         <div>
+//             {/* <UserContext.Consumer>
+//                 {
+//                    user=>{
+//                     return <h1>My Name is {user}</h1>
+//                    }
+//                 }
+//             </UserContext.Consumer> */}
             
-                <h1>Hey this is {user}</h1>
+//                 <h1>Hey this is {user}</h1>
             
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default ComponentC;
+// export default ComponentC;
