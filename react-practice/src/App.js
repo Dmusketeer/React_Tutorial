76350cf9743655db1737b6b18f5b72327ff98052
Dmuskeeter <tdheeraj9a@gmail.com>
2023-08-09 20:50:26 +0530

    signup form using class component

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
-import Home from "./components/Home";
-import Form from "./components/Form.js";
+import SignUpFrom from "./components/SignUpFrom";
+// import Form from "./components/Form.js";
 
 import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { count: 0 };
+    super();
   }
-  counter() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
   render() {
     return (
       <>
-        <h1>Table of My friends</h1>
-        <Home />
-        <Form />
+        <SignUpFrom />
       </>
     );
   }
