ed24c97f48387dd7ae06248f0f065c092321cbf7
Dmuskeeter <tdheeraj9a@gmail.com>
2023-08-13 16:53:25 +0530

    add recipe succefully

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Modal from "./components/Modal";
+// import Modal from "./components/Modal";
 import "./App.css";
 import { recipeLists } from "./Data/Data";
 
@@ -21,21 +21,82 @@ class App extends Component {
     this.setState({ showModal: true });
   };
 
-  handleCancel = () => {};
+  handleCancel = () => {
+    this.setState({ showModal: false });
+  };
 
-  handleOnChange = (e) => {};
+  handleOnChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // console.log(name.title);
+    this.setState({ [name]: value });
+  };
 
   //add recipe
-  handleAdd = () => {};
+  handleAdd = () => {
+    recipeLists.push({
+      title: this.state.title,
+      ingredients: this.state.ingredients.split(","),
+      instruction: this.state.instruction,
+    });
+    console.log(recipeLists);
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
 
+    // console.log();
+    this.setState({ showModal: false });
+  };
   //view recipe
   handleView = (i) => {};
 
   render() {
     return this.state.showModal ? (
-      <Modal />
+      <div>
+        {/* {this.props.children} */}
+        <form onSubmit={this.handleSubmit}>
+          <div className="pop">
+            <h1>Add Your Recipe</h1>
+            <h2>Title</h2>
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleOnChange}
+              className="inputFields"
+            />
+            <h2>Ingredients</h2>
+            <input
+              type="text"
+              className="inputFields"
+              name="ingredients"
+              value={this.state.ingredients}
+              onChange={this.handleOnChange}
+            />
+            <p>"*please seperate your ingredients with comma ,"</p>
+            <h2>Instruction</h2>
+            <textarea
+              name="instruction"
+              cols="30"
+              rows="10"
+              className="inputFields"
+              value={this.state.instruction}
+              onChange={this.handleOnChange}
+            ></textarea>
+            <button type="submit" className="add" onClick={this.handleAdd}>
+              Add
+            </button>
+            <button type="submit" className="add" onClick={this.handleCancel}>
+              Cancel
+            </button>
+            <p>----All fields are mandatory ----</p>
+          </div>
+        </form>
+      </div>
     ) : (
       <div className="headContent">
+        {/* //display the title ,search bar,add recipe button //display the recipe
+        cards //display modal */}
         <div>
           <h1 className="title">Recipe Book</h1>
           <input
@@ -54,7 +115,7 @@ class App extends Component {
         <div className="recipeContainer">
           {recipeLists.map((recipe) => {
             return (
-              <div className="recipeCard">
+              <div className="recipeCard" key={recipe.title}>
                 <h2>{recipe.title}</h2>
                 <button type="submit">view more</button>
               </div>
--- react-practice/src/components/Modal.js
+++ react-practice/src/components/Modal.js
@@ -1,33 +1,62 @@
 import React, { Component } from "react";
 
 class Modal extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event.target.title);
+    const formData = {
+      title: event.target.title.value,
+      instruction: event.target.instruction.value,
+      ingredients: event.target.ingredients.value,
+    };
+    this.props.handleAdd(formData);
+  };
   render() {
     return (
       <div>
         {/* {this.props.children} */}
-        <div className="pop">
-          <h1>Add Your Recipe</h1>
-          <h2>Title</h2>
-          <input type="text" id="title" className="inputFields " />
-          <h2>Ingredients</h2>
-          <input type="text" id="ingredients" className="inputFields" />
-          <p>"*please seperate your ingredients with comma ,"</p>
-          <h2>Instruction</h2>
-          <textarea
-            name="instruction"
-            id="instruction"
-            cols="30"
-            rows="10"
-            className="inputFields"
-          ></textarea>
-          <button type="submit" className="add">
-            Add
-          </button>
-          <button type="submit" className="add">
-            Cancel
-          </button>
-          <p>----All fields are mandatory ----</p>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="pop">
+            <h1>Add Your Recipe</h1>
+            <h2>Title</h2>
+            <input
+              type="text"
+              name="title"
+              value={this.props.data.title}
+              onChange={this.props.handleOnChange}
+              className="inputFields"
+            />
+            <h2>Ingredients</h2>
+            <input
+              type="text"
+              className="inputFields"
+              name="ingredients"
+              value={this.props.data.ingredients}
+              onChange={this.props.handleOnChange}
+            />
+            <p>"*please seperate your ingredients with comma ,"</p>
+            <h2>Instruction</h2>
+            <textarea
+              name="instruction"
+              cols="30"
+              rows="10"
+              className="inputFields"
+              value={this.props.data.instruction}
+              onChange={this.props.handleOnChange}
+            ></textarea>
+            <button type="submit" className="add">
+              Add
+            </button>
+            <button
+              type="submit"
+              className="add"
+              onClick={this.props.handleCancel}
+            >
+              Cancel
+            </button>
+            <p>----All fields are mandatory ----</p>
+          </div>
+        </form>
       </div>
     );
   }
